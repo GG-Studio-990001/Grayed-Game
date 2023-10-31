94ae8f7f0624128f8c37339d800e7b57ae9dbe61
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-11-01 03:48:22 +0900

    Test: Testcode UI(MVP) Example

---
+++ Assets/Scripts/Runtime/UI/TriviaGameView.cs
@@ -0,0 +1,17 @@
+namespace Runtime.UI
+{
+    public class TriviaGameView
+    {
+        public virtual void ShowPositiveFeedback()
+        {
+        }
+
+        public virtual void ShowWinFeedback()
+        {
+        }
+
+        public virtual void UpdateScore(int currentScore)
+        {
+        }
+    }
+}
