d4b37523e8fe27bc068488c5e2f25a892030c3be
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-11-06 07:17:52 +0900

    Feat: UI Presenter Factory, View, Data(SO)

---
+++ Assets/Scripts/Runtime/UI/PresenterFactory.cs
@@ -0,0 +1,14 @@
+using Runtime.UI.Settings;
+
+namespace Runtime.UI
+{
+    public class PresenterFactory
+    {
+        public static SettingsUIPresenter CreateSettingsUIPresenter(SettingsUIView view)
+        {
+            return new SettingsUIPresenter(view);
+        }
+        
+        // 앞으로 등장하는 모든 Presenter를 여기에 추가한다.
+    }
+}
--- Assets/Scripts/Runtime/UI/Settings/SettingsUIPresenter.cs
+++ Assets/Scripts/Runtime/UI/Settings/SettingsUIPresenter.cs
@@ -1,7 +1,29 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
 namespace Runtime.UI.Settings
 {
     public class SettingsUIPresenter
     {
+        private SettingsUIView _view;
+        private SettingsData _data;
+
+        public SettingsUIPresenter(SettingsUIView view)
+        {
+            _view = view;
+            
+            _data = Addressables.LoadAssetAsync<SettingsData>("SettingsData").Result;
+        }
         
+        public void OnViewCreated()
+        {
+            _view.SetMusicVolume(_data.MusicVolume);
+        }
+
+        public void OnMusicVolumeChanged(float volume)
+        {
+            _view.SetMusicVolume(volume);
+        }
     }
 }
