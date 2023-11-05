92ba37d7a44f06ecab3509e0750f0c380ef13375
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-11-06 06:24:35 +0900

    Test: MVP Test Setting

---
+++ Assets/Scripts/Tests/Editor/SettingsUIPresenterTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Assert = NUnit.Framework.Assert;
+
+[TestFixture]
+public class SettingsUIPresenterTests
+{
+    [Test]
+    public void DummyTest()
+    {
+        Assert.IsTrue(true);
+    }
+}
