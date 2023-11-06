34a3b190919dbbef7642bd97059d1f740804970c
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-11-06 20:56:18 +0900

    Feat: TopDown Player Anim

---
+++ Assets/Scripts/Runtime/CH1/Main/TopDownAnimation.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Runtime.CH1.Main
+{
+    public class TopDownAnimation
+    {
+        private Animator _animator;
+        private float _animationSpeed;
+        
+        public TopDownAnimation(Animator animator, float animationSpeed = 1.0f)
+        {
+            _animator = animator;
+            _animationSpeed = animationSpeed;
+            
+            _animator.speed = _animationSpeed;
+        }
+        
+        public void SetMovementAnimation(Vector2 movementInput)
+        {
+            // TODO 리터럴값 제거, 애니메이션 확장되는대로
+            if (movementInput == Vector2.zero)
+            {
+                _animator.SetBool("IsMoving", false);
+                return;
+            }
+            
+            _animator.SetBool("IsMoving", true);
+            
+            _animator.SetFloat("Horizontal", movementInput.x);
+            _animator.SetFloat("Vertical", movementInput.y);
+        }
+    }
+}
