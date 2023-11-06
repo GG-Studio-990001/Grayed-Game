0a59ca45131e7ea3cd2e448fb75424fa5e2d235b
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-11-06 20:20:28 +0900

    Feat: TopDownMovement Class modularization

--- Assets/Scripts/Runtime/CH1/Main/TopDownPlayer.cs
+++ Assets/Scripts/Runtime/CH1/Main/TopDownPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,33 +6,26 @@ namespace Runtime.CH1.Main
 {
     public class TopDownPlayer : MonoBehaviour
     {
-        public float moveSpeed = 5.0f;
-        private Vector2 movementInput;
+        // TODO 이거 데이터로 빼야함
+        [SerializeField] private float moveSpeed = 5.0f;
+        
+        private TopDownMovement _movement;
+        private Vector2 _movementInput;
+
+        private void Awake()
+        {
+            _movement = new TopDownMovement(moveSpeed, transform);
+        }
 
         private void OnMove(InputValue value)
         {
-            Vector2 inputVector = value.Get<Vector2>();
-            if (inputVector.magnitude > 1f)
-            {
-                return;
-            }
-
-            movementInput = new Vector2(inputVector.x, inputVector.y);
+            _movementInput = value.Get<Vector2>();
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
-            // 플레이어 이동 호출
-            MovePlayer();
+            _movement.Move(_movementInput);
         }
-
-        private void MovePlayer()
-        {
-            // 이동 벡터를 정규화하여 속도 곱하기
-            Vector2 movement = movementInput * moveSpeed * Time.deltaTime;
-
-            // 현재 위치에 이동 벡터를 더하여 이동
-            transform.Translate(movement);
-        }
+        
     }
 }
