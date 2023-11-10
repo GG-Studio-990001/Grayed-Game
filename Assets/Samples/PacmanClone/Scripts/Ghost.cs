af335511016aa3542d89871b79ee7a4632bb8a4f
우연 <bethelight730@gmail.com>
2023-11-10 20:23:09 +0900

    Feat: Make Pacman

---
+++ Assets/Samples/PacmanClone/Scripts/Ghost.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public int points = 200;
+}
---
+++ Assets/Samples/PacmanClone/Scripts/PacmanGameManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class PacmanGameManager : MonoBehaviour
+{
+    public Ghost[] ghosts;
+    public Pacman pacman;
+    public Transform pellets;
+
+    public int score { get; private set; }
+    public int lives { get; private set; }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (this.lives <= 0 && Input.anyKeyDown)
+        {
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        foreach (Transform pellet in this.pellets)
+        {
+            pellet.gameObject.SetActive(true);
+        }
+
+        ResetState();
+    }
+
+    private void ResetState()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(true);
+        }
+
+        this.pacman.gameObject.SetActive(true);
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
+            this.ghosts[i].gameObject.SetActive(false);
+        }
+
+        this.pacman.gameObject.SetActive(false);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public void GhosetEaten(Ghost ghost)
+    {
+        SetScore(this.score + ghost.points);
+    }
+
+    public void PacmanEaten()
+    {
+        this.pacman.gameObject.SetActive(false);
+
+        SetLives(this.lives - 1);
+
+        if (this.lives > 0)
+        {
+            Invoke(nameof(ResetState), 3.0f);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+}
