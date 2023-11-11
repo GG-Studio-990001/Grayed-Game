e1d7a169fe78fbbb99193de44111615271889359
우연 <bethelight730@gmail.com>
2023-11-12 01:02:19 +0900

    Feat: Ghost Frightend

--- Assets/Samples/PacmanClone/Scripts/GhostFrightend.cs
+++ Assets/Samples/PacmanClone/Scripts/GhostFrightend.cs
@@ -2,5 +2,108 @@ using UnityEngine;
 
 public class GhostFrightend : GhostBehavior
 {
-    
+    public SpriteRenderer body;
+    public SpriteRenderer eyes;
+    public SpriteRenderer blue;
+    public SpriteRenderer white;
+
+    public bool eaten { get; private set; }
+
+    public override void Enable(float duration)
+    {
+        base.Enable(duration);
+
+        this.body.enabled = false;
+        this.eyes.enabled = false;
+        this.blue.enabled = true;
+        this.white.enabled = false;
+
+        Invoke(nameof(Flash), duration / 2.0f);
+
+        // PowerPellet이 여러개기 때문에 확실하게 하기 위해 OnEnable ㄴㄴ
+    }
+
+    public override void Disable()
+    {
+        base.Disable();
+
+        this.body.enabled = true;
+        this.eyes.enabled = true;
+        this.blue.enabled = false;
+        this.white.enabled = false;
+    }
+
+    private void Flash()
+    {
+        if (!this.eaten)
+        {
+            this.blue.enabled = false;
+            this.white.enabled = true;
+            this.white.GetComponent<AnimatedSprite>().Restart();
+        }
+    }
+
+    private void Eaten()
+    {
+        this.eaten = true;
+        
+        Vector3 position = this.ghost.home.inside.position;
+        position.z = this.ghost.transform.position.z;
+        this.ghost.transform.position = position;
+
+        this.ghost.home.Enable(this.duration);
+
+        this.body.enabled = false;
+        this.eyes.enabled = true;
+        this.blue.enabled = false;
+        this.white.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        this.ghost.movement.speedMultiplier = 0.5f;
+        this.eaten = false;
+    }
+
+    private void OnDisable()
+    {
+        this.ghost.movement.speedMultiplier = 1f;
+        this.eaten = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            if (this.enabled)
+            {
+                Eaten();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && this.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float maxDistance = float.MinValue;
+
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float distance = (this.ghost.target.position - newPosition).sqrMagnitude; // magnitude is slow and costly
+
+                if (distance > maxDistance)
+                {
+                    direction = availableDirection;
+                    maxDistance = distance;
+                }
+            }
+
+            this.ghost.movement.SetDirection(direction);
+        }
+    }
 }
