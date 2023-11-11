0aa547013af781271832ee65dbf1506109695648
우연 <bethelight730@gmail.com>
2023-11-11 23:43:12 +0900

    Feat: Ghost Scatter & Chase

--- Assets/Samples/PacmanClone/Scripts/GhostChase.cs
+++ Assets/Samples/PacmanClone/Scripts/GhostChase.cs
@@ -2,5 +2,33 @@ using UnityEngine;
 
 public class GhostChase : GhostBehavior
 {
-    
+    private void OnDisable()
+    {
+        this.ghost.scatter.Enable();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Node node = other.GetComponent<Node>();
+
+        if (node != null && this.enabled && !this.ghost.frightend.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float distance = (this.ghost.target.position - newPosition).sqrMagnitude; // magnitude is slow and costly
+
+                if (distance < minDistance)
+                {
+                    direction = availableDirection;
+                    minDistance = distance;
+                }
+            }
+
+            this.ghost.movement.SetDirection(direction);
+        }
+    }
 }
