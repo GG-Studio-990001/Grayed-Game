62f6371cd1faf672e7258eee1623d46b7e9ffe2c
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-11-13 19:11:29 +0900

    Feat: Input System Rename

---
+++ Assets/Scripts/Runtime/CH1/Main/CH1GameController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Runtime.CH1.Main
+{
+    public class CH1GameController : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameSettingUI;
+        
+        GameOverControls _gameOverControls;
+
+        private void Awake()
+        {
+            _gameOverControls = new GameOverControls();
+        }
+        
+        private void OnEnable()
+        {
+            _gameOverControls.Enable();
+            
+            _gameOverControls.UI.GameSetting.performed += ctx => OnGameSetting();
+        }
+        
+        private void OnDisable()
+        {
+            _gameOverControls.Disable();
+        }
+        
+        private void Start()
+        {
+            SetAllUIActiveFalse();
+        }
+        
+        private void SetAllUIActiveFalse()
+        {
+            gameSettingUI.SetActive(false);
+        }
+        
+        private void OnGameSetting()
+        {
+            gameSettingUI.SetActive(gameSettingUI.activeSelf == false);
+        }
+    }
+}
