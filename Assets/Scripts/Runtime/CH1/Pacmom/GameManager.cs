d72281b3c91b1d66694e543a045dc598c932a5a7
우연 <bethelight730@gmail.com>
2023-11-19 21:55:26 +0900

    Feat: Rapley Eat Coin

---
+++ Assets/Scripts/Runtime/CH1/Pacmom/GameManager.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+namespace Runtime.CH1.Pacmom
+{
+    public class GameManager : MonoBehaviour
+    {
+        public Rapley rapley;
+        public Transform coins;
+
+        public int rapleyScore { get; private set; }
+
+        private void Start()
+        {
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            SetRapleyScore(0);
+
+            foreach (Transform coin in coins)
+            {
+                coin.gameObject.SetActive(true);
+            }
+        }
+
+        private void SetRapleyScore(int score)
+        {
+            rapleyScore = score;
+        }
+
+        public void CoinEaten(Coin coin)
+        {
+            coin.gameObject.SetActive(false);
+            SetRapleyScore(rapleyScore + 1);
+
+            if (!HasRemainingCoins())
+            {
+                Debug.Log("Game Clear! 3초 뒤 재시작");
+                Invoke("NewGame", 3f);
+            }
+        }
+
+        private bool HasRemainingCoins()
+        {
+            foreach (Transform coin in coins)
+            {
+                if (coin.gameObject.activeSelf)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
