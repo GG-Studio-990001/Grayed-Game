cf6a7bc8ccb46d300a3d62cb6829e4fb925f6745
우연 <bethelight730@gmail.com>
2023-11-19 21:16:49 +0900

    Feat: Rapley Movement

--- Assets/Scripts/Runtime/CH1/Pacmom/Rapley.cs
+++ Assets/Scripts/Runtime/CH1/Pacmom/Rapley.cs
@@ -1,18 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Rapley : MonoBehaviour
+namespace Runtime.CH1.Pacmom
 {
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Rapley : MonoBehaviour
     {
-        
-    }
+        public Rigidbody2D rigid { get; private set; }
+        public Vector2 direction { get; private set; }
+        public Vector2 nextDirection { get; private set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Speeds")]
+        public float speed = 8f;
+        public float speedMultiplier = 1f; // 팩맘의 청소기 습득 시 속도 변화를 위한 변수
+
+        [Header("Else")]
+        public LayerMask obstacleLayer;
+
+
+        private void Awake()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            direction = new Vector2(1f, 0f); // initial direction 오른쪽
+            nextDirection = Vector2.zero;
+        }
+
+        private void Update()
+        {
+            if (nextDirection != Vector2.zero)
+            {
+                SetDirection(nextDirection);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            // 길 너비와 몸 지름이 같기 때문에 rigidbody를 주체로 움직임
+            Vector2 position = rigid.position;
+            Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
+
+            rigid.MovePosition(position + translation);
+        }
+
+        private void OnMove(InputValue value)
+        {
+            Vector2 inputDirection = value.Get<Vector2>();
+            if (inputDirection != Vector2.zero)
+            {
+                nextDirection = inputDirection;
+            }
+        }
+
+        public void SetDirection(Vector2 direction)
+        {
+            if (!CheckRoadBlocked(direction))
+            {
+                this.direction = direction;
+                nextDirection = Vector2.zero;
+            }
+        }
+
+        public bool CheckRoadBlocked(Vector2 direction)
+        {
+            // 방향에 길이 막혀있으면 true 반환
+            // 몸집이 있기 때문에 box로 검출
+            RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.5f, 0f, direction, 1.0f, obstacleLayer);
+
+            return hit.collider != null;
+        }
     }
 }
