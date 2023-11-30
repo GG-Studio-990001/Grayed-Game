3bd5ed64758be6a0b40b2a8a9c904309f30e728d
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-11-30 20:32:09 +0900

    Refactor: Lamda Func

--- Assets/Scripts/Runtime/CH1/Main/TopDownPlayer.cs
+++ Assets/Scripts/Runtime/CH1/Main/TopDownPlayer.cs
@@ -22,9 +22,7 @@ namespace Runtime.CH1.Main
         private void Awake()
         {
             _movement = new TopDownMovement(moveSpeed, transform);
-            
             _animation = new TopDownAnimation(GetComponent<Animator>(), animSpeed);
-            
             _interaction = new TopDownInteraction(transform, LayerMask.GetMask(Interaction));
         }
         
@@ -33,20 +31,10 @@ namespace Runtime.CH1.Main
             _animation.SetMovementAnimation(_movementInput);
         }
 
-        private void FixedUpdate()
-        {
-            _movement.Move(_movementInput);
-        }
+        private void FixedUpdate() => _movement.Move(_movementInput);
 
-        private void OnMove(InputValue value)
-        {
-            _movementInput = value.Get<Vector2>();
-        }
+        private void OnMove(InputValue value) => _movementInput = value.Get<Vector2>();
         
-        private void OnInteraction()
-        {
-            Debug.Log("Interaction"+_movement.Direction);
-            _interaction.Interact(_movement.Direction);
-        }
+        private void OnInteraction() => _interaction.Interact(_movement.Direction);
     }
 }
