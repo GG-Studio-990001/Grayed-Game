2a442891a5804ffafe3b8f9a8a7a4e01aab6244b
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-12-02 11:52:08 +0900

    Feat: Dialogue System Demo

--- Assets/Scripts/Runtime/CH1/Main/NpcInteraction.cs
+++ Assets/Scripts/Runtime/CH1/Main/NpcInteraction.cs
@@ -2,14 +2,18 @@ using Runtime.CH1.Main.Interface;
 using Runtime.Common.Presentation;
 using System;
 using UnityEngine;
+using Yarn.Unity;
 
 namespace Runtime.CH1.Main
 {
     public class NpcInteraction : MonoBehaviour, IInteractive
     {
+        [SerializeField] private string talkToNode = "";
+        [SerializeField] private DialogueRunner dialogueRunner;
+
         public bool Interact()
         {
-            Debug.Log("Test");
+            dialogueRunner.StartDialogue(talkToNode);
             
             return true;
         }
