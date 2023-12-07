6012177ba739128d9b58bd891296538c4b0f1e04
우연 <bethelight730@gmail.com>
2023-12-08 00:59:52 +0900

    Feat: Apply AI To Pacmom Movement

--- Assets/Scripts/Runtime/CH1/Pacmom/Pacmom.cs
+++ Assets/Scripts/Runtime/CH1/Pacmom/Pacmom.cs
@@ -19,6 +19,9 @@ namespace Runtime.CH1.Pacmom
         [SerializeField]
         private Sprite[] dieSpr;
 
+        [SerializeField]
+        private Transform[] enemys;
+
         private void Awake()
         {
             spriteAnim = GetComponent<SpriteAnimation>();
@@ -96,21 +99,91 @@ namespace Runtime.CH1.Pacmom
         {
             Step step = other.GetComponent<Step>();
 
-            if (step != null)
+            if (step == null)
+                return;
+
+            Vector2 direction = Vector2.zero;
+
+            foreach (Transform enemy in enemys)
             {
+                // TO DO: 적 여러마리일 때 가장 가까운 적을 기준으로 구현 (먼지유령 추가 후)
+                float distance = (enemy.position - transform.position).sqrMagnitude;
+
+                if (distance < 30f)
+                {
+                    if (!isVacuumMode)
+                    {
+                        RunAwayFromEnemy(enemy, step);
+                    }
+                    else
+                    {
+                        ChaseEnemy(enemy, step);
+                    }
+                }
+            }
+
+            // TO DO: 적이 가까이 있지 않을 때 가까운 코인 감지
+
+            if (direction == Vector2.zero) // 랜덤 이동
+            {
+                Debug.Log("아무데나!");
+
                 int index = Random.Range(0, step.availableDirections.Count);
 
-                // 거꾸로 가기 방지
                 if (step.availableDirections[index] == -1 * movement.direction && step.availableDirections.Count > 1)
                 {
-                    index++; // 랜덤으로 대신 +1
+                    index++;
 
                     if (index >= step.availableDirections.Count)
                         index = 0;
                 }
-
-                movement.SetNextDirection(step.availableDirections[index]);
+                direction = step.availableDirections[index];
             }
+
+            movement.SetNextDirection(direction);
+        }
+
+        private Vector2 RunAwayFromEnemy(Transform enemy, Step step)
+        {
+            Debug.Log("도망가!");
+
+            Vector2 direction = Vector2.zero;
+            float maxDistance = float.MinValue;
+
+            foreach (Vector2 availableDirection in step.availableDirections)
+            {
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float newDistance = (enemy.position - newPosition).sqrMagnitude;
+
+                if (newDistance > maxDistance)
+                {
+                    maxDistance = newDistance;
+                    direction = availableDirection;
+                }
+            }
+
+            return direction;
+        }
+
+        private Vector2 ChaseEnemy(Transform enemy, Step step)
+        {
+            Debug.Log("쫓아가!");
+            Vector2 direction = Vector2.zero;
+            float minDistance = float.MaxValue;
+
+            foreach (Vector2 availableDirection in step.availableDirections)
+            {
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float newDistance = (enemy.position - newPosition).sqrMagnitude;
+
+                if (newDistance < minDistance)
+                {
+                    minDistance = newDistance;
+                    direction = availableDirection;
+                }
+            }
+
+            return direction;
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
