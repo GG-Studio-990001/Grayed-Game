ede24146137ce79eb13f18b469fa2d4897279b54
우연 <bethelight730@gmail.com>
2023-12-21 21:16:54 +0900

    Feat: Temporary Buttons For Restart&Exit

--- Assets/Scripts/Runtime/SceneSystem.cs
+++ Assets/Scripts/Runtime/SceneSystem.cs
@@ -8,5 +8,13 @@ namespace Runtime
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
         }
+
+        public void OnExitButtonClicked()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+            Application.Quit();
+        }
     }
 }
