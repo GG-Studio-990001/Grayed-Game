fbb292204a73562ebc049533b77678c9e44f6e19
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-01-01 18:20:47 +0900

    Todo: To change camera function

--- Assets/Scripts/Runtime/PlayerFollowCamera.cs
+++ Assets/Scripts/Runtime/PlayerFollowCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Camera -> Cinemachine -> Virtual Camera -> Follow -> Player (수정 예정)
 public class PlayerFollowCamera : MonoBehaviour
 {
     [SerializeField] private Transform playerTransform;
