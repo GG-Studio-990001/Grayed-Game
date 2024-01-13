2a81df4a6a46c33547724b31d6a2b277148d262c
우연 <bethelight730@gmail.com>
2024-01-13 22:07:09 +0900

    Feat: Opening Timeline 1

--- Assets/Scripts/Runtime/CH1/Pacmom/PMTimeline.cs
+++ Assets/Scripts/Runtime/CH1/Pacmom/PMTimeline.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Yarn;
+using Yarn.Unity;
 
 namespace Runtime.CH1.Pacmom
 {
@@ -15,8 +17,10 @@ namespace Runtime.CH1.Pacmom
         private GameObject Door;
         [SerializeField]
         private GameObject Sparkle;
+        [SerializeField]
+        private GameObject[] dialouges;
 
-        public void OpeningStart()
+        public void OpeningStart_1()
         {
             UI.SetActive(false);
             Door.SetActive(true);
@@ -24,14 +28,29 @@ namespace Runtime.CH1.Pacmom
             ControlEnable(false);
         }
 
-        public void OpeningFinish()
+        public void OpeningEnd_1()
+        {
+            Sparkle.SetActive(false);
+            ControlEnable(true);
+            foreach (var d in dialouges)
+            {
+                d.SetActive(true);
+            }
+        }
+
+        /*
+        public void OpeningStart_2()
+        {
+            ControlEnable(false);
+        }
+
+        public void OpeningEnd_2()
         {
             UI.SetActive(true);
             Door.SetActive(false);
-            Sparkle.SetActive(false);
             ControlEnable(true);
             Controller.StartGame();
-        }
+        }*/
 
         private void ControlEnable(bool control)
         {
