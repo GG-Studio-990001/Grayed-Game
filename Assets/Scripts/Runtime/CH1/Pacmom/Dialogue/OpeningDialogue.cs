7f5beb0aa873515ce00d697aa0ca687bd18ec7b6
우연 <bethelight730@gmail.com>
2024-02-18 16:15:45 +0900

    Refactor: OpeningDialogue
    enum 사용
    enum은 null 안되는 걸로 앎

--- Assets/Scripts/Runtime/CH1/Pacmom/Dialogue/OpeningDialogue.cs
+++ Assets/Scripts/Runtime/CH1/Pacmom/Dialogue/OpeningDialogue.cs
@@ -8,10 +8,17 @@ using Yarn;
 
 namespace Runtime.CH1.Pacmom
 {
+    public enum Speaker
+    {
+        dustA,
+        dustB,
+        rapley,
+        none
+    }
+
     public class OpeningDialogue : DialogueViewBase
     {
         private DialogueRunner runner;
-        private readonly int dustA = 0, dustB = 1, rapley = 2;
 
         [SerializeField]
         private SoundSystem soundSystem;
@@ -51,59 +58,56 @@ namespace Runtime.CH1.Pacmom
 
             string speaker = dialogueLine.CharacterName;
 
-            if (speaker == GlobalConst.DustAStr || speaker == GlobalConst.DustBStr)
-            {
-                if (speaker == GlobalConst.DustAStr)
-                    ResizeSpeechBubble(dustA);
-                else
-                    ResizeSpeechBubble(dustB);
-            }
+            if (speaker == GlobalConst.DustAStr)
+                ResizeSpeechBubble(Speaker.dustA);
+            else if (speaker == GlobalConst.DustBStr)
+                ResizeSpeechBubble(Speaker.dustB);
 
             onDialogueLineFinished();
         }
 
-        private void ResizeSpeechBubble(int speaker)
+        private void ResizeSpeechBubble(Speaker speaker)
         {
             float height = (line.text.Length <= 12 ? 200f : 225f);
 
-            if (speaker == 0)
+            if (speaker == Speaker.dustA)
                 speechBubbleA.sizeDelta = new Vector2(speechBubbleA.sizeDelta.x, height - 12.26f);
-            else
+            else if (speaker == Speaker.dustB)
                 speechBubbleB.sizeDelta = new Vector2(speechBubbleB.sizeDelta.x, height);
         }
 
         public void DustASpeak()
         {
-            ShowSpeechBubble(dustA);
-            SetLinePos(dustA);
+            ShowSpeechBubble(Speaker.dustA);
+            SetLinePos(Speaker.dustA);
         }
 
         public void DustBSpeak()
         {
-            ShowSpeechBubble(dustB);
-            SetLinePos(dustB);
+            ShowSpeechBubble(Speaker.dustB);
+            SetLinePos(Speaker.dustB);
         }
 
         public void RapleySpeak()
         {
-            ShowSpeechBubble(rapley);
+            ShowSpeechBubble(Speaker.rapley);
         }
 
         public void OpeningDialogueFin()
         {
             soundSystem.StopSFX();
-            ShowSpeechBubble();
+            ShowSpeechBubble(Speaker.none);
             timeline_2.SetActive(true);
         }
 
-        private void SetLinePos(int speaker)
+        private void SetLinePos(Speaker speaker)
         {
-            float xPos = (speaker == 0 ? -474 : 474);
+            float xPos = (speaker == Speaker.dustA ? -474 : 474);
 
             line.transform.localPosition = new Vector3(xPos, line.transform.localPosition.y, line.transform.localPosition.z);
         }
 
-        private void ShowSpeechBubble(int speaker = -1)
+        private void ShowSpeechBubble(Speaker speaker)
         {
             speechBubbleA.gameObject.SetActive(false);
             speechBubbleB.gameObject.SetActive(false);
@@ -111,13 +115,13 @@ namespace Runtime.CH1.Pacmom
 
             switch (speaker)
             {
-                case 0: // dust A
+                case Speaker.dustA:
                     speechBubbleA.gameObject.SetActive(true);
                     break;
-                case 1: // dust B
+                case Speaker.dustB:
                     speechBubbleB.gameObject.SetActive(true);
                     break;
-                case 2: // rapley
+                case Speaker.rapley:
                     rapleyBubble.SetActive(true);
                     break;
             }
