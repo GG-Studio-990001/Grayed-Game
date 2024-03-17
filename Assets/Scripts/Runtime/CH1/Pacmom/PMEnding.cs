d815371c1fb7799ddcf26a78cc34cb2c7f5e5ae1
우연 <bethelight730@gmail.com>
2024-03-17 22:57:17 +0900

    Data / Ending

--- Assets/Scripts/Runtime/CH1/Pacmom/PMEnding.cs
+++ Assets/Scripts/Runtime/CH1/Pacmom/PMEnding.cs
@@ -7,19 +7,19 @@ namespace Runtime.CH1.Pacmom
     public class PMEnding : MonoBehaviour
     {
         [SerializeField]
-        private SceneSystem sceneSystem;
+        private SceneSystem _sceneSystem;
         [SerializeField]
-        private PMShader PMShader;
+        private PMShader _Shader;
         [SerializeField]
-        private GameObject Timeline_3;
+        private GameObject _timeline_3;
         [SerializeField]
-        private SpriteControl[] spriteControls = new SpriteControl[6];
+        private SpriteControl[] _spriteControls = new SpriteControl[6];
         public bool isGameClear { get; private set; } // 저장 필요
 
         public void RapleyWin()
         {
             Debug.Log("라플리 승리");
-            Timeline_3.SetActive(true);
+            _timeline_3.SetActive(true);
             isGameClear = true;
         }
 
@@ -32,29 +32,29 @@ namespace Runtime.CH1.Pacmom
 
         public void GetAllNormalSprite()
         {
-            for (int i = 0; i < spriteControls.Length; i++)
+            for (int i = 0; i < _spriteControls.Length; i++)
             {
-                spriteControls[i].GetNormalSprite();
+                _spriteControls[i].GetNormalSprite();
             }
         }
 
         public void GetAllVacuumModeSprite()
         {
-            for (int i = 0; i < spriteControls.Length; i++)
+            for (int i = 0; i < _spriteControls.Length; i++)
             {
-                spriteControls[i].GetVacuumModeSprite();
+                _spriteControls[i].GetVacuumModeSprite();
             }
         }
 
         IEnumerator ToMain()
         {
             Time.timeScale = 0;
-            PMShader.ChangeBleedAmount();
+            _Shader.ChangeBleedAmount();
 
             yield return new WaitForSecondsRealtime(3f);
 
             Time.timeScale = 1f;
-            sceneSystem.LoadScene("Main");
+            _sceneSystem.LoadScene("Main");
         }
     }
 }
