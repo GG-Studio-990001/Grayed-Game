143c7d659b072f0d112fbcf318c8d4a320fb591d
우연 <bethelight730@gmail.com>
2024-03-23 22:46:45 +0900

    Refactor: PMShader

--- Assets/Scripts/Runtime/CH1/Pacmom/PMShader.cs
+++ Assets/Scripts/Runtime/CH1/Pacmom/PMShader.cs
@@ -6,34 +6,31 @@ namespace Runtime.CH1.Pacmom
     public class PMShader : MonoBehaviour
     {
         [SerializeField]
-        public Volume volume;
-        private Bleed _m_bleed;
+        private Volume _volume;
+        private Bleed _bleed;
 
         private void Start()
         {
-            if (volume is null)
+            if (_volume is null)
                 return;
 
-            volume.profile.TryGet(out _m_bleed);
+            _volume.profile.TryGet(out _bleed);
 
-            if (_m_bleed is null)
+            if (_bleed is null)
             {
                 Debug.Log("Add Glitch1 effect to your Volume component to make Manipulation Example work");
                 return;
             }
 
-            _m_bleed.active = true;
+            _bleed.active = true;
         }
 
         public void ChangeBleedAmount()
         {
-            //Null check
-            if (volume is null)
-                return;
-            if (_m_bleed is null)
+            if (_volume is null || _bleed is null)
                 return;
 
-            _m_bleed.bleedAmount.value = 10f;
+            _bleed.bleedAmount.value = 10f;
         }
     }
 }
