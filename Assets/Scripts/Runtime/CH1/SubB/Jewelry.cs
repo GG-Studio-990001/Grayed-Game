e19570358f2f97f1e0123572af14c4794094e945
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-04-28 02:10:14 +0900

    Fix: Random Del

--- Assets/Scripts/Runtime/CH1/SubB/Jewelry.cs
+++ Assets/Scripts/Runtime/CH1/SubB/Jewelry.cs
@@ -7,22 +7,24 @@ namespace Runtime.CH1.SubB
 {
     public class Jewelry : MonoBehaviour
     {
-        [field:SerializeField] public JewelryType JewelryType { get; set; }
+        [field: SerializeField] public JewelryType JewelryType { get; set; }
         [SerializeField] private Transform spriteTransform;
         [SerializeField] private float moveTime = 0.5f;
         [SerializeField] private float pushLimitTime = 1.0f;
 
         public ThreeMatchPuzzleController Controller { get; set; }
         public Tilemap Tilemap { get; private set; }
-        
-        private Vector3 _firstPosition;
+
+        private Vector3 _orignalPosition;
+        private JewelryType _originalType;
         private IMovement _movement;
         private float _pushTime;
 
         private void Awake()
         {
             Tilemap = GetComponentInParent<Tilemap>();
-            _firstPosition = transform.position;
+            _orignalPosition = transform.position;
+            _originalType = JewelryType;
             _movement = new JewelryMovement(this.transform, spriteTransform, moveTime, Tilemap);
         }
 
@@ -32,19 +34,20 @@ namespace Runtime.CH1.SubB
             {
                 return;
             }
-            
+
             _pushTime += Time.deltaTime;
-            
+
             if (other.gameObject.CompareTag(GlobalConst.PlayerStr))
             {
                 if (_pushTime > pushLimitTime)
                 {
                     _pushTime = 0f;
-                    
-                    Vector2 playerDetectionPos = new Vector2(other.transform.position.x, other.transform.position.y + other.collider.offset.y);
+
+                    Vector2 playerDetectionPos = new Vector2(other.transform.position.x,
+                        other.transform.position.y + other.collider.offset.y);
 
                     Vector2 direction = (transform.position - (Vector3)playerDetectionPos);
-                    
+
                     direction.x = Mathf.Abs(direction.x) > Mathf.Abs(direction.y) ? Mathf.Sign(direction.x) : 0f;
                     direction.y = Mathf.Abs(direction.y) > Mathf.Abs(direction.x) ? Mathf.Sign(direction.y) : 0f;
 
@@ -56,15 +59,20 @@ namespace Runtime.CH1.SubB
                 }
             }
         }
-        
-        private void OnCollisionExit2D(Collision2D other) => _pushTime = 0f;
-        
-        public void ResetPosition() => transform.position = _firstPosition;
 
-        public void DestroyJewelry()
+        private void OnCollisionExit2D(Collision2D other) => _pushTime = 0f;
+
+        public void ResetPosition()
         {
-            Invoke(nameof(SetActiveFalse), 0.1f);
+            transform.position = _orignalPosition;
+            JewelryType = _originalType;
+            gameObject.SetActive(true);
+        }
+
+    public void DestroyJewelry()
+        {
             JewelryType = JewelryType.Disappear;
+            Invoke(nameof(SetActiveFalse), 0.1f);
             // 이펙트
         }
         
