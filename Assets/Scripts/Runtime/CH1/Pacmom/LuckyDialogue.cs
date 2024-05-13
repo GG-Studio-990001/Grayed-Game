e8901c0c806a7ea0c1cf614b54dc4b0be12c32da
우연 <bethelight730@gmail.com>
2024-05-13 20:51:34 +0900

    Feat: Lucky Animations

--- Assets/Scripts/Runtime/CH1/Pacmom/LuckyDialogue.cs
+++ Assets/Scripts/Runtime/CH1/Pacmom/LuckyDialogue.cs
@@ -1,13 +1,15 @@
 using DG.Tweening;
+using Runtime.Luck;
 using UnityEngine;
 using Yarn.Unity;
+using static PlasticGui.PlasticTableColumn;
 
 namespace Runtime.CH1.Pacmom
 {
     public class LuckyDialogue : DialogueViewBase
     {
         private DialogueRunner _runner;
-        [SerializeField] private GameObject _lucky;
+        [SerializeField] private Lucky _lucky;
         [SerializeField] private GameObject _bubble;
         [SerializeField] private Vector3 _outPosition;
         [SerializeField] private Vector3 _inPosition;
@@ -17,22 +19,43 @@ namespace Runtime.CH1.Pacmom
             _lucky.transform.localPosition = _outPosition;
 
             _runner = GetComponent<DialogueRunner>();
-            _runner.AddCommandHandler("LuckyWalkIn", LuckyWalkIn);
-            _runner.AddCommandHandler("LuckyWalkOut", LuckyWalkOut);
+            _runner.AddCommandHandler("WalkIn", WalkIn);
+            _runner.AddCommandHandler("WalkOut", WalkOut);
             _runner.AddCommandHandler<bool>("ActiveBubble", ActiveBubble);
+            _runner.AddCommandHandler("Idle", Idle);
+            _runner.AddCommandHandler("Pointing", Pointing);
+            _runner.AddCommandHandler("Showing", Showing);
         }
 
-        // 럭키 이동 (시작 위치, 끝 위치)
-
-        private void LuckyWalkIn()
+        private void WalkIn()
         {
+            _lucky.Anim.SetAnimation("Walking"); // enum으로 변경
             _lucky.transform.DOLocalMove(_inPosition, 3f).SetEase(Ease.Linear);
             // 여기서 wait 호출 어케하지?
         }
 
-        private void LuckyWalkOut()
+        private void WalkOut()
         {
+            _lucky.Anim.SetAnimation("Walking");
+            _lucky.SetFlipX(true);
             _lucky.transform.DOLocalMove(_outPosition, 3f).SetEase(Ease.Linear);
+        }
+
+        private void Idle()
+        {
+            _lucky.SetFlipX(false);
+            _lucky.Anim.ResetAnim();
+            Debug.Log("Idle");
+        }
+
+        private void Pointing()
+        {
+            _lucky.Anim.SetAnimation("Pointing");
+        }
+
+        private void Showing()
+        {
+            _lucky.Anim.SetAnimation("Showing");
         }
 
         private void ActiveBubble(bool active)
---
+++ Assets/Scripts/Runtime/Lucky/LuckyAnimation.cs
@@ -0,0 +1,64 @@
+using Runtime.Interface;
+using UnityEngine;
+
+namespace Runtime.Luck
+{
+    public class LuckyAnimation
+    {
+        private readonly Animator _animator;
+        //private static readonly int Standing = Animator.StringToHash(IsStanding);
+        private static readonly int Walking = Animator.StringToHash(IsWalking);
+        private static readonly int Pointing = Animator.StringToHash(IsPointing);
+        private static readonly int Showing = Animator.StringToHash(IsShowing);
+        private static readonly int Clapping = Animator.StringToHash(IsClapping);
+
+        //private const string IsStanding = "IsStanding";
+        private const string IsWalking = "IsWalking";
+        private const string IsPointing = "IsPointing";
+        private const string IsShowing = "IsShowing";
+        private const string IsClapping = "IsClapping";
+        // fish
+
+        public LuckyAnimation(Animator animator)
+        {
+            _animator = animator;
+            _animator.speed = 2f;
+        }
+
+        public void ResetAnim()
+        {
+            _animator.speed = 2f;
+            _animator.SetBool(Walking, false);
+            _animator.SetBool(Pointing, false);
+            _animator.SetBool(Showing, false);
+            _animator.SetBool(Clapping, false);
+        }
+
+        public void SetAnimation(string stateName, Vector2 direction = default)
+        {
+            ResetAnim();
+
+            switch (stateName)
+            {
+                case "Standing":
+                    break;
+                case "Walking":
+                    _animator.speed = 5f;
+                    _animator.SetBool(Walking, true);
+                    break;
+                case "Pointing":
+                    _animator.SetBool(Pointing, true);
+                    break;
+                case "Showing":
+                    _animator.SetBool(Showing, true);
+                    break;
+                case "Clapping":
+                    _animator.SetBool(Clapping, true);
+                    break;
+                default:
+                    Debug.LogError("Invalid State");
+                    break;
+            }
+        }
+    }
+}
