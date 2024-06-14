d5668afa96b42d61cddfc8d9b9f5302c3e359294
우연 <bethelight730@gmail.com>
2024-06-14 17:01:56 +0900

    Feat: Apply Lucky BGM

--- Assets/Scripts/Runtime/CH2/Main/CH2Controller.cs
+++ Assets/Scripts/Runtime/CH2/Main/CH2Controller.cs
@@ -7,7 +7,7 @@ namespace Runtime.CH2
     {
         void Start()
         {
-            Managers.Sound.Play(Sound.BGM, "Mamago_BGM_1");
+            Managers.Sound.Play(Sound.BGM, "[Ch1] Lucky_BGM_3");
         }
     }
 }
