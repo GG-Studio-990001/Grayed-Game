41bd9fa6371026041bcc548d3a6ad071e5048b88
우연 <bethelight730@gmail.com>
2024-06-16 15:00:03 +0900

    Feat: Trivial Things

--- Assets/Scripts/Runtime/CH1/Main/Dialogue/TimelineController.cs
+++ Assets/Scripts/Runtime/CH1/Main/Dialogue/TimelineController.cs
@@ -1,4 +1,3 @@
-using Runtime.Data.Original;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -11,37 +10,37 @@ namespace Runtime.CH1.Main.Dialogue
         [SerializeField] private TimelineAsset[] _timelineAssets;
         [field: SerializeField] public PlayableDirector PlayableDirector { get; private set; }
 
-        //public PlayerData playerData;
+        // 안 쓰는 중
+        // public PlayerData playerData;
 
-        /*
         public void PlayTimeline()
         {
             //PlayTimeline(playerData.quarter.minor);
         }
 
-        public void PlayTimeline(int minor)
-        {
-            if (_timelineAssets.Length <= minor)
-            {
-                Debug.LogError($"TimelineAsset with minor {minor} not found");
-                return;
-            }
-            PlayableDirector.playableAsset = _timelineAssets[minor];
-            PlayableDirector.Play();
-        }*/
+        //public void PlayTimeline(int minor)
+        //{
+        //    if (_timelineAssets.Length <= minor)
+        //    {
+        //        Debug.LogError($"TimelineAsset with minor {minor} not found");
+        //        return;
+        //    }
+        //    PlayableDirector.playableAsset = _timelineAssets[minor];
+        //    PlayableDirector.Play();
+        //}
 
-        public void PlayTimeline(string timelineName)
-        {
-            for (int i = 0; i < _timelineAssets.Length; i++)
-            {
-                if (_timelineAssets[i].name == timelineName)
-                {
-                    PlayableDirector.playableAsset = _timelineAssets[i];
-                    PlayableDirector.Play();
-                    return;
-                }
-            }
-            Debug.LogError($"TimelineAsset with name {timelineName} not found");
-        }
+        //public void PlayTimeline(string timelineName)
+        //{
+        //    for (int i = 0; i < _timelineAssets.Length; i++)
+        //    {
+        //        if (_timelineAssets[i].name == timelineName)
+        //        {
+        //            PlayableDirector.playableAsset = _timelineAssets[i];
+        //            PlayableDirector.Play();
+        //            return;
+        //        }
+        //    }
+        //    Debug.LogError($"TimelineAsset with name {timelineName} not found");
+        //}
     }
 }
