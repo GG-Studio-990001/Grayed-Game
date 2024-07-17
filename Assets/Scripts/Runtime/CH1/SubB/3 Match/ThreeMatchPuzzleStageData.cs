1ea601640de7c4be9f493a339ef86b17ff8be6d1
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2024-07-18 08:46:29 +0900

    Fix: 3M Data Load

---
+++ Assets/Scripts/Runtime/CH1/SubB/3 Match/ThreeMatchPuzzleStageData.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runtime.CH1.SubB._3_Match
+{
+    [Serializable]
+    public class ThreeMatchPuzzleStageData
+    {
+        public bool isClear;
+        public List<Vector2> jewelryPositions;
+    }
+}
