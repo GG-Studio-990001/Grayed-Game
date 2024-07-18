54aa7f65b0db0538f9fa631250b303e30dbb3750
우연 <bethelight730@gmail.com>
2024-07-19 01:45:04 +0900

    Feat: MainScene Buttons - After Intro, After Pacmom

--- Assets/Scripts/Runtime/Main/MainController.cs
+++ Assets/Scripts/Runtime/Main/MainController.cs
@@ -12,6 +12,8 @@ namespace Runtime.Main
             Managers.Data.InGameKeyBinder.GameControlReset();
         }
 
+        // Hard Cordings
+
         public void NewGame()
         {
             Managers.Data.NewGame();
@@ -22,6 +24,64 @@ namespace Runtime.Main
         {
             Managers.Data.LoadGame();
             sceneSystem.LoadScene("CH1");
+        }
+
+        public void GoPacmom()
+        {
+            Managers.Data.NewGame();
+            sceneSystem.LoadScene("Pacmom");
+        }
+
+        public void GoCH2()
+        {
+            Managers.Data.NewGame();
+            sceneSystem.LoadScene("CH2");
+        }
+
+        public void AfterIntro()
+        {
+            Managers.Data.NewGame();
+            Managers.Data.Scene = 1;
+            Managers.Data.SceneDetail = 1;
+            Managers.Data.SaveGame();
+            Managers.Data.LoadGame();
+            sceneSystem.LoadScene("CH1");
+        }
+
+        public void AfterPacmom()
+        {
+            Managers.Data.NewGame();
+            Managers.Data.Scene = 1;
+            Managers.Data.SceneDetail = 1;
+            Managers.Data.MeetLucky = true;
+            Managers.Data.IsPacmomPlayed = true;
+            Managers.Data.IsPacmomCleared = true;
+            Managers.Data.PacmomCoin = 300;
+            Managers.Data.SaveGame();
+            Managers.Data.LoadGame();
+            sceneSystem.LoadScene("CH1");
+        }
+
+        public void After3Match()
+        {
+            Managers.Data.NewGame();
+            Managers.Data.Scene = 3;
+            Managers.Data.SceneDetail = 1;
+            Managers.Data.MeetLucky = true;
+            Managers.Data.IsPacmomPlayed = true;
+            Managers.Data.IsPacmomCleared = true;
+            Managers.Data.PacmomCoin = 300;
+            Managers.Data.Is3MatchEntered = true;
+            Managers.Data.Is3MatchCleared = true;
+            Managers.Data.SaveGame();
+            Managers.Data.LoadGame();
+            sceneSystem.LoadScene("CH1");
+        }
+
+        public void AfterSLG()
+        {
+            Managers.Data.NewGame();
+
         }
     }
 }
