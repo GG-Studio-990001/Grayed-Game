6739194dab035798237dd1b85983f6ac4b9d6b47
우연 <bethelight730@gmail.com>
2024-07-22 01:55:08 +0900

    작업중

--- Assets/Scripts/Runtime/Middle/TurnOff.cs
+++ Assets/Scripts/Runtime/Middle/TurnOff.cs
@@ -2,12 +2,19 @@ using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 
 public class TurnOff : MonoBehaviour
 {
+    // TODO: 클래스명 더 적합한 걸로 변경..
+
     [SerializeField] private Volume _volume;
+    [SerializeField] private RectTransform _blackLeft;
+    [SerializeField] private RectTransform _blackRight;
     private CinematicBars _cinematicBar;
     private Noise _noise;
+
     void Start()
     {
         if (_volume != null)
@@ -23,12 +30,20 @@ public class TurnOff : MonoBehaviour
     {
         _cinematicBar.amount.value = 0f;
 
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1f);
 
-        DOTween.To(() => _cinematicBar.amount.value, x => _cinematicBar.amount.value = x, 1f, 0.5f);
+        DOTween.To(() => _cinematicBar.amount.value, x => _cinematicBar.amount.value = x, 0.5f, 0.5f);
+        yield return new WaitForSeconds(0.5f);
 
+        CinematicBar2();
         yield return new WaitForSeconds(0.5f);
 
         _noise.enable.value = false;
+    }
+
+    private void CinematicBar2()
+    {
+        _blackLeft.DOMoveX(0, 0.5f).SetEase(Ease.Linear);
+        _blackRight.DOMoveX(0, 0.5f).SetEase(Ease.Linear);
     }
 }
