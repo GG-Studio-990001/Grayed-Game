fe8a92f8440a123e0c84ee3319ef82ec36b6f375
Hyun2and <52280545+jeongopo@users.noreply.github.com>
2024-08-05 22:28:36 +0900

    Feat : Rollback Building FX

--- Assets/Scripts/Runtime/CH1/SubB/SLG/UI/SLGBuildingListWindow.cs
+++ Assets/Scripts/Runtime/CH1/SubB/SLG/UI/SLGBuildingListWindow.cs
@@ -68,7 +68,7 @@ public class SLGBuildingListWindow : MonoBehaviour
         else 
         {
             transform.anchoredPosition = StartPos;
-            transform.DOLocalMove(EndPos, AnimTime).SetEase(Ease.OutBounce);
+            transform.DOLocalMove(EndPos, AnimTime);
         }
     }
 
