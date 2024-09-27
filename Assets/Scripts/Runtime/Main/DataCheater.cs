3fe890fa81e1d2961dd3dca7869cb87b270ea6f4
우연 <bethelight730@gmail.com>
2024-09-27 22:58:37 +0900

    Refactor: Specify namespace

---
+++ Assets/Scripts/Runtime/Main/DataCheater.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Runtime.Main
+{
+    namespace Runtime.ETC
+    {
+        public class DataCheater : MonoBehaviour
+        {
+        }
+    }
+}
--- Assets/Scripts/Runtime/Main/TestData.cs
+++ Assets/Scripts/Runtime/Main/TestData.cs
@@ -1,29 +1,32 @@
-public class TestData
+namespace Runtime.Main
 {
-    public void DataAfterIntro()
+    public class TestData
     {
-        Managers.Data.Chapter = 1;
-        Managers.Data.Stage = 1;
-        Managers.Data.Scene = 1;
-        Managers.Data.SceneDetail = 1;
-    }
+        public void DataAfterIntro()
+        {
+            Managers.Data.Chapter = 1;
+            Managers.Data.Stage = 1;
+            Managers.Data.Scene = 1;
+            Managers.Data.SceneDetail = 1;
+        }
 
-    public void DataAfterPacmom()
-    {
-        DataAfterIntro();
-        Managers.Data.MeetLucky = true;
-        Managers.Data.IsPacmomPlayed = true;
-        Managers.Data.IsPacmomCleared = true;
-        Managers.Data.PacmomCoin = 300;
-    }
+        public void DataAfterPacmom()
+        {
+            DataAfterIntro();
+            Managers.Data.MeetLucky = true;
+            Managers.Data.IsPacmomPlayed = true;
+            Managers.Data.IsPacmomCleared = true;
+            Managers.Data.PacmomCoin = 300;
+        }
 
-    public void DataAfter3Match()
-    {
-        DataAfterPacmom();
-        Managers.Data.Stage = 2;
-        Managers.Data.Scene = 3;
-        Managers.Data.SceneDetail = 1;
-        Managers.Data.Is3MatchEntered = true;
-        Managers.Data.Is3MatchCleared = true;
+        public void DataAfter3Match()
+        {
+            DataAfterPacmom();
+            Managers.Data.Stage = 2;
+            Managers.Data.Scene = 3;
+            Managers.Data.SceneDetail = 1;
+            Managers.Data.Is3MatchEntered = true;
+            Managers.Data.Is3MatchCleared = true;
+        }
     }
 }
