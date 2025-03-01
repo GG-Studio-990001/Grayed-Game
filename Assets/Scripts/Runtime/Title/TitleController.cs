db58c25fbf10b311f63fffda031cb6940d06c19c
우연 <bethelight730@gmail.com>
2025-03-01 20:01:36 +0900

    Fix: CH1 Title Not Working

--- Assets/Scripts/Runtime/Title/TitleController.cs
+++ Assets/Scripts/Runtime/Title/TitleController.cs
@@ -7,6 +7,7 @@ namespace Runtime.CH1.Title
     public class TitleController : MonoBehaviour
     {
         [SerializeField] private SceneSystem _sceneSystem;
+        [SerializeField] private FadeController _fadeController;
         [SerializeField] private GameObject[] _chObjs = new GameObject[3];
 
         private void Start()
@@ -18,11 +19,13 @@ namespace Runtime.CH1.Title
 
             if (ch <= 1)
             {
+                _fadeController.StartFadeIn();
                 _chObjs[0].SetActive(true);
                 Managers.Sound.Play(Sound.BGM, $"Title/Title_BGM_CH1");
             }
             else
             {
+                // ch2, ch3
                 _chObjs[ch - 1].SetActive(true);
                 Managers.Sound.Play(Sound.BGM, $"Title/Title_BGM_CH{ch}");
             }
