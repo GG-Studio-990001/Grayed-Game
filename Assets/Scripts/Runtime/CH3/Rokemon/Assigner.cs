99d3fc8d882d97f367863c617cca4bb3a836e9e2
우연 <bethelight730@gmail.com>
2025-03-10 20:02:47 +0900

    Refactor: Assigner

--- Assets/Scripts/Runtime/CH3/Rokemon/Assigner.cs
+++ Assets/Scripts/Runtime/CH3/Rokemon/Assigner.cs
@@ -5,19 +5,23 @@ namespace Runtime.CH3.Rokemon
 {
     public class Assigner : MonoBehaviour
     {
+        [Header("==스킬==")]
         [SerializeField] private Skill[] _skills;
         [SerializeField] private TextMeshProUGUI _typeTxt;
         [SerializeField] private TextMeshProUGUI _nameTxt;
         [SerializeField] private TextMeshProUGUI _descTxt;
-        [SerializeField] private TextMeshProUGUI _curLvTxt;
-        [SerializeField] private TextMeshProUGUI _newLvTxt;
-        [SerializeField] private TextMeshProUGUI _leftLvTxt;
+        [Header("==할당==")]
+        [SerializeField] private TextMeshProUGUI _curLvTxt; // 기존 Lv
+        [SerializeField] private TextMeshProUGUI _tmpLvTxt; // 변경중인 임시 Lv
+        [Header("==잔여==")]
+        [SerializeField] private TextMeshProUGUI _leftLvTxt; // 잔여 Lv
         private int _skillIdx;
         private int _curLv;
-        private int _newLv;
+        private int _tmpLv;
         private int _maxLv;
         private int _leftLv = 40;
         private int _usedLv = 0;
+        private int _totalUsedLv = 30; // 지금까지 사용한 Lv 합 (습득 조건 위함)
 
         public void UpdateAssignPage(int idx)
         {
@@ -28,9 +32,9 @@ namespace Runtime.CH3.Rokemon
             _curLvTxt.text = _skills[_skillIdx].CurLv.ToString();
 
             _curLv = _skills[_skillIdx].CurLv;
-            _newLv = _skills[_skillIdx].CurLv;
+            _tmpLv = _skills[_skillIdx].CurLv;
             _maxLv = _skills[_skillIdx].MaxLv;
-            UpdateNewLvTxt();
+            UpdateTmpLvTxt();
 
             _usedLv = 0;
         }
@@ -38,32 +42,32 @@ namespace Runtime.CH3.Rokemon
         #region Lv 할당 확인 및 텍스트 업데이트
         public bool HasChanged()
         {
-            return _newLv != _curLv;
+            return _tmpLv != _curLv;
         }
 
-        private void UpdateNewLvTxt()
+        private void UpdateTmpLvTxt() // 임시로 할당한 Lv 값 (아직 저장 X)
         {
-            _newLvTxt.text = _newLv.ToString();
+            _tmpLvTxt.text = _tmpLv.ToString();
         }
         #endregion
 
         #region Lv 할당(조절) 버튼
         public void MinBtn()
         {
-            _newLv = _curLv;
+            _tmpLv = _curLv;
             _usedLv = 0;
-            UpdateNewLvTxt();
+            UpdateTmpLvTxt();
         }
 
         public void MaxBtn()
         {
             int availableRp = _leftLv - _usedLv; // 사용 가능한 남은 Lv
-            int newLv = Mathf.Clamp(_newLv + availableRp, _curLv, _maxLv);
+            int newLv = Mathf.Clamp(_tmpLv + availableRp, _curLv, _maxLv);
 
-            _usedLv += newLv - _newLv; // 실제 사용한 값만큼 업데이트
-            _newLv = newLv;
+            _usedLv += newLv - _tmpLv; // 실제 사용한 값만큼 업데이트
+            _tmpLv = newLv;
 
-            UpdateNewLvTxt();
+            UpdateTmpLvTxt();
         }
 
         public void UpBtn(int val)
@@ -72,40 +76,55 @@ namespace Runtime.CH3.Rokemon
             if (val > availableRp)
                 val = availableRp;
 
-            int newLv = Mathf.Clamp(_newLv + val, _curLv, _maxLv);
-            _usedLv += newLv - _newLv;
-            _newLv = newLv;
-            UpdateNewLvTxt();
+            int newLv = Mathf.Clamp(_tmpLv + val, _curLv, _maxLv);
+            _usedLv += newLv - _tmpLv;
+            _tmpLv = newLv;
+            UpdateTmpLvTxt();
         }
 
         public void DownBtn(int val)
         {
             val = Mathf.Clamp(val, 0, _usedLv); // 반환할 수 있는 범위를 초과하지 않도록 보정
 
-            int newLv = Mathf.Clamp(_newLv - val, _curLv, _maxLv); // 최소 _curLv 유지
-            _usedLv -= _newLv - newLv; // 감소한 값만큼 _usedLv 복구
-            _newLv = newLv;
+            int newLv = Mathf.Clamp(_tmpLv - val, _curLv, _maxLv); // 최소 _curLv 유지
+            _usedLv -= _tmpLv - newLv; // 감소한 값만큼 _usedLv 복구
+            _tmpLv = newLv;
 
-            UpdateNewLvTxt();
+            UpdateTmpLvTxt();
         }
         #endregion
 
         #region Lv 저장
         public void SaveLv()
         {
-            _skills[_skillIdx].CurLv = _newLv;
+            _skills[_skillIdx].CurLv = _tmpLv;
             _skills[_skillIdx].SetLvTxt();
 
-            _leftLv -= _usedLv;
-            UpdateLeftLv();
+            UpdateTotalUsedLv(_usedLv);
+            UpdateLeftLv(_usedLv);
         }
         #endregion
 
-        #region 잔여 Lv 업데이트
-        private void UpdateLeftLv()
+        private void UpdateTotalUsedLv(int used)
         {
+            int lastUsedLv = _totalUsedLv;
+            _totalUsedLv += used;
+
+            if (lastUsedLv < 30 && _totalUsedLv >= 30)
+            {
+                // 매력 스킬 습득 조건 달성
+            }
+        }
+
+        private void UpdateLeftLv(int used)
+        {
+            _leftLv -= used;
             _leftLvTxt.text = $"{_leftLv} / 70";
         }
-        #endregion
+
+        private void RevertLeftLv()
+        {
+            // TODO: 잔여를 실시간으로 변경, 저장하지 않으면 복구 가능하도록 짜야함
+        }
     }
 }
