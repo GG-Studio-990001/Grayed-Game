060dfe77072b7e18abf4d9251d49df3fa1a93514
우연 <bethelight730@gmail.com>
2025-04-28 19:51:44 +0900

    Feat: Roll The Dice Test

--- Assets/Scripts/Runtime/CH3/TRPG/DiceRoll.cs
+++ Assets/Scripts/Runtime/CH3/TRPG/DiceRoll.cs
@@ -4,25 +4,45 @@ namespace Runtime.CH3.DnD
 {
     public class DiceRoll : MonoBehaviour
     {
-        [SerializeField] private float _maxRandomForceValue, _startRollingForce;
+        // [SerializeField] private float _maxRandomForceValue, _startRollingForce;
+        private Vector3 _initPos;
+        private Quaternion _initRot;
         public int DiceFaceNum;
         Rigidbody _body;
-        private float _forceX, _forceY, _forceZ;
+        [SerializeField] private float _forceX, _forceY, _forceZ;
 
         private void Awake()
         {
+            //_initRot = transform.rotation;
+            _initPos = transform.position;
             Initialize();
+        }
+
+        public void RollDiceTest()
+        {
+            Initialize();
+            _body.isKinematic = false;
+            _body.AddForce(350, 500, -150);
+            _body.AddTorque(0, 0, -20);
+        }
+
+        public void RollDiceTest2()
+        {
+            Initialize();
+            _body.isKinematic = false;
+            _body.AddForce(500, 500, -50);
+            _body.AddTorque(0, 0, -10);
         }
 
         public void RollDice()
         {
-            _body.isKinematic = false;
-            _forceX = Random.Range(0, _maxRandomForceValue);
-            _forceY = Random.Range(0, _maxRandomForceValue);
-            _forceZ = Random.Range(0, _maxRandomForceValue);
+            //_body.iskinematic = false;
+            //_forcex = random.range(0, _maxrandomforcevalue);
+            //_forcey = random.range(0, _maxrandomforcevalue);
+            //_forcez = random.range(0, _maxrandomforcevalue);
 
-            _body.AddForce(Vector3.up * _startRollingForce);
-            _body.AddTorque(_forceX, _forceY, _forceZ);
+            //_body.addforce(vector3.up * _startrollingforce);
+            //_body.addtorque(_forcex, _forcey, _forcez);
         }
 
         private void Initialize()
@@ -30,7 +50,9 @@ namespace Runtime.CH3.DnD
             DiceFaceNum = -1;
             _body = GetComponent<Rigidbody>();
             _body.isKinematic = true;
-            transform.rotation = new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), 0);
+            // transform.SetPositionAndRotation(_initPos, _initRot);
+            transform.position = _initPos;
+            transform.rotation = new Quaternion(Random.Range(0, 30), 0, 0, 0);
         }
     }
 }
