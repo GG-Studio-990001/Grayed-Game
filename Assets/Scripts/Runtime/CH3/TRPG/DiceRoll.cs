a1e4f3a5fa212f7857c6c38b74fd1a45d1fdb776
우연 <bethelight730@gmail.com>
2025-05-16 22:37:05 +0900

    Feat: Dice Roll 1

--- Assets/Scripts/Runtime/CH3/TRPG/DiceRoll.cs
+++ Assets/Scripts/Runtime/CH3/TRPG/DiceRoll.cs
@@ -12,7 +12,7 @@ namespace Runtime.CH3.TRPG
 
         private void Awake()
         {
-            Physics.gravity = new Vector3(0, -9.81f * 3, 0);
+            Physics.gravity = new Vector3(0, -9.81f * 3, 0); // TODO: CH3씬에 합칠 때 2.5D에 중력 영향 안가도록 설정
 
             // _initRot = transform.rotation;
             _initPos = transform.position;
@@ -21,21 +21,25 @@ namespace Runtime.CH3.TRPG
 
         public void RollDice()
         {
-            Initialize();
+            // Initialize();
+            
+            Debug.Log(this.name + " " + transform.localEulerAngles);
             _body.isKinematic = false;
 
             float t = 4f;
             _body.AddForce(_forceX * t, _forceY * t, _forceZ * t);
         }
 
-        private void Initialize()
+        public void Initialize()
         {
             DiceFaceNum = -1;
             _body = GetComponent<Rigidbody>();
             _body.isKinematic = true;
 
             // transform.position = _initPos;
-            Quaternion rot = Quaternion.Euler(Random.Range(0, 181), Random.Range(0, 181), Random.Range(0, 181));
+            // Quaternion rot = Quaternion.Euler(Random.Range(0, 181), Random.Range(0, 181), Random.Range(0, 181));
+            Quaternion rot = Quaternion.Euler(Random.Range(0, 2) == 0 ? 90 : -90, 0, 72 * Random.Range(1, 6));
+            // Quaternion rot = Quaternion.Euler(0, 0, 0);
             transform.SetPositionAndRotation(_initPos, rot);
         }
     }
