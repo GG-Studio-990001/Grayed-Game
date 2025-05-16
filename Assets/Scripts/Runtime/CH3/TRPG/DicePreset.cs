026c8e90925a85a4b3f1fff70feb177c333353af
우연 <bethelight730@gmail.com>
2025-05-16 23:45:15 +0900

    Feat: Dice Roll 2

---
+++ Assets/Scripts/Runtime/CH3/TRPG/DicePreset.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Runtime.CH3.TRPG
+{
+    public class DicePreset : MonoBehaviour
+    {
+        
+        
+    }
+}
--- Assets/Scripts/Runtime/CH3/TRPG/DiceRoll.cs
+++ Assets/Scripts/Runtime/CH3/TRPG/DiceRoll.cs
@@ -5,42 +5,52 @@ namespace Runtime.CH3.TRPG
     public class DiceRoll : MonoBehaviour
     {
         private Vector3 _initPos;
-        // private Quaternion _initRot;
         public int DiceFaceNum;
         Rigidbody _body;
         [SerializeField] private float _forceX, _forceY, _forceZ;
+        [SerializeField] private Vector3[] _presets;
 
         private void Awake()
         {
             Physics.gravity = new Vector3(0, -9.81f * 3, 0); // TODO: CH3씬에 합칠 때 2.5D에 중력 영향 안가도록 설정
 
-            // _initRot = transform.rotation;
+            _body = GetComponent<Rigidbody>();
             _initPos = transform.position;
+
             Initialize();
         }
 
         public void RollDice()
         {
             // Initialize();
-            
-            Debug.Log(this.name + " " + transform.localEulerAngles);
+            // SetValue();
+
+            Debug.Log(this.name + " Vector3" + transform.localEulerAngles);
+
             _body.isKinematic = false;
 
             float t = 4f;
             _body.AddForce(_forceX * t, _forceY * t, _forceZ * t);
         }
 
-        public void Initialize()
+        private void Initialize()
         {
             DiceFaceNum = -1;
-            _body = GetComponent<Rigidbody>();
             _body.isKinematic = true;
+        }
 
-            // transform.position = _initPos;
-            // Quaternion rot = Quaternion.Euler(Random.Range(0, 181), Random.Range(0, 181), Random.Range(0, 181));
-            Quaternion rot = Quaternion.Euler(Random.Range(0, 2) == 0 ? 90 : -90, 0, 72 * Random.Range(1, 6));
-            // Quaternion rot = Quaternion.Euler(0, 0, 0);
+        private void SetValue()
+        {
+            int val = Random.Range(0, 10);
+            Quaternion rot = Quaternion.Euler(_presets[val]);
             transform.SetPositionAndRotation(_initPos, rot);
+            Debug.Log(this.name + " 목표값: " + val);
+        }
+
+        public void Test()
+        {
+            Initialize();
+            SetValue();
         }
     }
 }
