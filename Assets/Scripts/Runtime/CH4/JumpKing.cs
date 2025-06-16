9e406f43e980608e73c3768c326a4c36db572d60
우연 <bethelight730@gmail.com>
2025-06-17 00:18:29 +0900

    Feat: JumpKing

---
+++ Assets/Scripts/Runtime/CH4/JumpKing.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using InputOld = UnityEngine.Input;
+
+namespace Runtime.CH4
+{
+    public class JumpKing : MonoBehaviour
+    {
+        [SerializeField] private float _walkSpeed;
+        [SerializeField] private bool _isGrounded;
+        [SerializeField] private LayerMask _groundMask;
+
+        [SerializeField] private PhysicsMaterial2D _normalMat, _BounceMat;
+        [SerializeField] private bool _canJump = true;
+        [SerializeField] private float _jumpValue = 0.0f;
+
+        private float _legLen = 1.4f;
+        private Vector2 _feetSize = new(1.5f, 0.4f);
+
+        private float _moveInput;
+        private Rigidbody2D _rb;
+
+
+        void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            _moveInput = InputOld.GetAxisRaw("Horizontal");
+
+            if (_jumpValue == 0f && _isGrounded)
+            {
+                _rb.velocity = new Vector2(_moveInput * _walkSpeed, _rb.velocity.y);
+            }
+
+            _isGrounded = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - _legLen),
+                _feetSize, 0f, _groundMask);
+
+            if (_jumpValue > 0)
+            {
+                _rb.sharedMaterial = _BounceMat;
+            }
+            else
+            {
+                _rb.sharedMaterial = _normalMat;
+            }
+
+            if (InputOld.GetKey(KeyCode.Space) && _isGrounded && _canJump)
+            {
+                _jumpValue += 0.1f;
+            }
+
+            if (InputOld.GetKeyDown(KeyCode.Space) && _isGrounded && _canJump)
+            {
+                _rb.velocity = new Vector2(0f, _rb.velocity.y);
+            }
+
+            if (_jumpValue >= 20f && _isGrounded)
+            {
+                float tmpX = _moveInput * _walkSpeed;
+                float tmpY = _jumpValue;
+                _rb.velocity = new Vector2(tmpX, tmpY);
+                Invoke(nameof(ResetJump), 0.2f);
+            }
+
+            if (InputOld.GetKeyUp(KeyCode.Space))
+            {
+                if (_isGrounded)
+                {
+                    _rb.velocity = new Vector2(_moveInput * _walkSpeed, _jumpValue);
+                    _jumpValue = 0f;
+                }
+                _canJump = true;
+            }
+        }
+
+        void ResetJump()
+        {
+            _canJump = false;
+            _jumpValue = 0f;
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y - _legLen), _feetSize);
+        }
+    }
+}
