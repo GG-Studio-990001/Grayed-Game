492508f0e6dc39f36d91e6a791124a1098d05832
Siwon <fhrhdls@naver.com>
2025-06-17 18:23:29 +0900

    Feat: Add Excel Impoter

---
+++ Assets/ExcelImporter/Runtime/ExcelAssetAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public class ExcelAssetAttribute : Attribute
+{
+	public string AssetPath { get; set; }
+	public string ExcelName { get; set; }
+	public bool LogOnImport { get; set; }
+}
