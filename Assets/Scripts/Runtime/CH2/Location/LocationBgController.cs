c595f3f3e30f0b264735cf347564d566944d980a
우연 <bethelight730@gmail.com>
2025-11-03 21:39:37 +0900

    Feat: Modify CH2 Dialogue Turn0~2

--- Assets/Scripts/Runtime/CH2/Location/LocationBgController.cs
+++ Assets/Scripts/Runtime/CH2/Location/LocationBgController.cs
@@ -34,7 +34,7 @@ namespace Runtime.CH2.Location
             { "Entrance", 0 },        // 마을 입구
             { "Square", 1 },          // 광장
             { "Temple", 2 },          // 신전
-            { "Statue", 3 },          // 달러 동상
+            { "Statue", 3 },          // 달러 동상 // 삭제
             { "Cave", 4 },            // 동굴
             { "Base", 5 },            // 기지
             { "Storage", 6 },         // 창고
@@ -42,10 +42,10 @@ namespace Runtime.CH2.Location
             { "InTemple", 8 },       // 신전 내부
             { "TempleRoom", 9 },     // 신전 방
             { "Backstreet", 10 },      // 골목길
-            { "DollarHouse", 11 },    // 달러의 집
-            { "Passage", 12 },        // 통로
-            { "InStatue", 13 },      // 달러 동상 내부
-            { "StatueCrack", 14 }     // 달러 동상 틈새
+            { "DollarHouse", 11 },    // 달러의 집 // 삭제
+            { "Passage", 12 },        // 통로 // 삭제
+            { "InStatue", 13 },      // 달러 동상 내부 // 삭제
+            { "StatueCrack", 14 }     // 달러 동상 틈새 // 삭제
         };
 
         public void SetLocationUI()
