fcb2476e0c22f83b54df213c669307fa073a2769
우연 <bethelight730@gmail.com>
2026-01-07 00:05:19 +0900

    Feat: Add CH4 Temp Bgm

--- Assets/Scripts/Runtime/CH4/Common/CH4KeyBinder.cs
+++ Assets/Scripts/Runtime/CH4/Common/CH4KeyBinder.cs
@@ -1,6 +1,6 @@
-using Runtime.CH1.Main.Player;
 using Runtime.Common.View;
 using UnityEngine;
+using Runtime.ETC;
 
 namespace Runtime.CH4
 {
@@ -8,10 +8,20 @@ namespace Runtime.CH4
     {
         [SerializeField] private PlatformerPlayer _player;
         [SerializeField] private SettingsUIView _settingsUIView;
+        [SerializeField] private int _chapterNum;
 
         private void Start()
         {
             InitKeyBinding();
+
+            if (_chapterNum == 1)
+            {
+                Managers.Sound.Play(Sound.BGM, "CH1/Main(Cave)_BGM");
+            }
+            else if (_chapterNum == 2)
+            {
+                Managers.Sound.Play(Sound.BGM, "CH2/BGM_06_Micael's Riddle");
+            }
         }
 
         private void InitKeyBinding()
